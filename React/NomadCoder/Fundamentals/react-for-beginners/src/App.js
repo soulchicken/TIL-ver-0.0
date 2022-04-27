f36dfa95e82ff9ca5def42007811579b334748e4
soulchicken <soulfever01@naver.com>
2022-04-27 22:47:20 +0900

    TIL (Today I Learned) 2022.04.27 (수)
    ### 1. 부트캠프 - JAVA
    - static, instance 변수의 차이. public, private의 차이가 사실상 메인이었다.
    - 기록할 것은 딱히 못느끼고 있다. JS를 배울 때는 모든게 새로워서 막 신나서 기록했는데.
      이것까지 기록할 이유가 있나? 싶은 것들은 안 적게 된다. 의무감으로 숙제처럼 노트정리를 할 순 없다.
    
    ### 2. 클린코드 독서 챌린지
    - 함수를 작성할 때에 처음부터 잘 작성하긴 힘들다. 다듬고 나누고 붙이고 줄이는 과정에서 예쁜 코드가 완성된다.
    
    ### 3. React - Nomad Coder
    - npm에서 prop-types를 받으면 props에서 내려오는 친구들의 타입을 지정할 수 있다.
      좋은 발견....인가 타입스크립트 공부하는게 나은 것인가.

--- React/NomadCoder/Fundamentals/react-for-beginners/src/App.js
+++ React/NomadCoder/Fundamentals/react-for-beginners/src/App.js
@@ -1,8 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1 className={styles.title}>Hello</h1>
+        <Button text={"Continue"}/>
       </header>
     </div>
   );
