3732ed052d571815afdd1cbab38c9969ee135c0c
soulchicken <soulfever01@naver.com>
2022-04-29 20:34:59 +0900

    TIL (Today I Learned) 2022.04.29 (금)
    ### 1. 클린코드 독서 챌린지
    - 주석 챕터를 다 읽었다. 저자가 주석을 극혐하는게 느껴진다.
      싫다는 말은 아니고. 충분히 공감이 가는 내용이었다.
    - **좋은 주석은 없는 주석이다.**
    
    ### 2. React - Nomad Coder
    - 주말까지만 공부하면 슬슬 강의가 끝날 것 같다.
    - 오늘 공부의 메인은 useEffect. re-render를 막아주는 친구.

--- React/NomadCoder/Fundamentals/react-for-beginners/src/App.js
+++ React/NomadCoder/Fundamentals/react-for-beginners/src/App.js
@@ -1,11 +1,24 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only Once!");
+  }
+  useEffect(() => {
+    console.log("CALL THE API...");
+  },[]);
   return (
     <div className="App">
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <header className="App-header">
-        <h1 className={styles.title}>Hello</h1>
-        <Button text={"Continue"}/>
+        <h1 className={styles.title}>{counter}</h1>
+        <button onClick={onClick}>click me</button>
       </header>
     </div>
   );
